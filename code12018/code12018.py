cf7df2df39dd46b7214fa9a362809ee0adeba710
Arroyo <adarroyo@FTDCORP.NET>
2018-01-02 17:39:15 -0600

    Add project files.

---
+++ code12018/code12018.py
@@ -0,0 +1,24 @@
+print("will output to console window,useful for debugging");
+#concatenation:combinations of things
+#you can use the + or , but this will need a space in front
+print('can do this',5)
+
+#You cannot use the "+" to join strings with ints or floats, you must use the ",".
+#print('cannot do this:'+5)#will error
+
+# / this is the escape character
+#example 1
+x ="He said, \"HELLO THERE!\""
+print(x);
+#other exaples
+
+#print('can't do this)
+
+print('you\'ll have sucess here');
+
+print("you'll have a good chance here too");
+
+#comments are hekpful to other developers
+print('Hello world');
+#or
+print("will print as long as the quotes match")
